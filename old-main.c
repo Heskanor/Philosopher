75a599e3e44566342642419755a93a7385db3df2
Ahmed  Shite <ashite@e0r2p11.1337.ma>
2021-10-05 13:12:21 +0100

    no message

--- old-main.c
+++ old-main.c
@@ -292,7 +292,7 @@ gettimeofday(&start, NULL);
 gettimeofday(&stop, NULL);
 printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
 
-
+gcc -g -pthread old-main.c && ./a.out 5 1948032830 6000 600 5     
 philo < 201
 times > 60 ms
 
